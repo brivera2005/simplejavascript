3c63c34d745fe8b8f855f1d4e29224e73525f617
Benjamin <34975594+brivera2005@users.noreply.github.com>
2023-02-28 08:24:53 -0600

    first commit

---
+++ Javascript Practice/multiplicationTables.js
@@ -0,0 +1,14 @@
+//Nested loop was able to print all 100 numbers for a multiplication table from 1 to 10, but having trouble figuring out how to display as a grid.
+
+for(i=1; i<11; i++) 
+    for(j=1; j<11; j++)
+    console.log(i*j)
+
+//To do this, we need to create a table variable and make it be an empty array. Also create row and column arrays.
+
+let table = [];
+for(i=1; i<11; i++) 
+    for(j=1; j<11; j++)
+    console.log(i*j)
+    
+        
